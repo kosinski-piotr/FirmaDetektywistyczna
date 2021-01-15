f2567355485a336b66fbb9588d9a6d9ed5464b7d
jurasafc <jurasafc@gmail.com>
2021-01-15 18:15:28 +0100

    Wygląd+Logowanie

--- FirmaDetektywistyczna/index.php
+++ FirmaDetektywistyczna/index.php
@@ -5,18 +5,49 @@
     <title>Firma detektywistyczna Herkules</title>
     <link rel="stylesheet" href="css/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+    <style type="text/css">
+        .nav-dark {
+            width:100%
+        }
+    </style>
 </head>
 <body>
-	<form action="login.php" method="post">
+	<div class="user-container">
+		<form action="login.php" method="post">
 	
-		Login: <br /> <input type="text" name="login" /> <br />
-		Hasło: <br /> <input type="password" name="haslo" /> <br /><br />
-		<input type="submit" value="Zaloguj się" />
+			Login: <input type="text" name="login" />
+			Hasło: <input type="password" name="haslo" /> 
+			<input type="submit" value="Zaloguj się" />
 	
-	</form>
+		</form>
+	</div>
 
-    <?php
+    <div class="top">
+        <div class="name">Herkules!</div>
+    </div>
+
+    <div class="navbar">
+        <button
+            class="nav-dark"
+            onclick="window.location.href='index.php'">
+            Strona główna
+        </button>
+    </div>
+
+    <div class="text">
+        <b>ZALOGUJ SIĘ ABY MIEĆ DOSTĘP DO WSZYSTKICH MOŻLIWOSCI!</b><br /><br />
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </div>
+        <?php
 	if(isset($_SESSION['blad']))	echo $_SESSION['blad'];
-    ?>
+        ?>
 </body>
 </html>
--- FirmaDetektywistyczna/zalogowany.php
+++ FirmaDetektywistyczna/zalogowany.php
@@ -13,22 +13,60 @@ if (!isset($_SESSION['zalogowany']))
 <html lang="pl">
 <head>
     <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Oliwski</title>
 </head>
 
 <body>
+    <div class="user-container">
+        <a class="user-link" href="profile.php">
+            <?php
+            echo "<p>Zalogowany jako ".$_SESSION['Imie'].' '.$_SESSION['Nazwisko'];
+            ?>
+        </a>
+    </div>
 
-    <?php
+    <div class="top">
+        <div class="name">Herkules!</div>
 
-	echo "<p>Witaj ".$_SESSION['user'].'! [ <a href="logout.php">Wyloguj się!</a> ]</p>';
-	echo "<p><b>Imie</b>: ".$_SESSION['Imie'];
-	echo " | <b>Nazwisko</b>: ".$_SESSION['Nazwisko'];
-	echo " | <b>Uprawnienia</b>: ".$_SESSION['Uprawnienia']."</p>";
+        <div class="space"></div>
 
-	echo "<p><b>ID</b>: ".$_SESSION['id'];
+        <div class="envelope-container">
+            <img src="images/Envelope-icon.png" alt="koperta" class="envelope" />
+        </div>
 
-    ?>
+        <form action="logout.php" method="get">
+            <input class="button "type="submit" value="Wyloguj się" />
+        </form>
+    </div>
 
+    <div class="navbar">
+        <button
+            class="nav-dark"
+            onclick="window.location.href='stronaGlowna.html'">
+            Strona główna
+        </button>
+
+        <button class="nav" onclick="window.location.href='stronaGlowna.html'">
+            Kontakt
+        </button>
+
+        <button class="nav" onclick="window.location.href='stronaGlowna.html'">
+            Przeglądaj Usługi
+        </button>
+    </div>
+
+    <div class="text">
+        orem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </div>
 </body>
 </html>
